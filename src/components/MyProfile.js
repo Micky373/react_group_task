0db50a120e2fd87e92d61124a933a8cdbae2ec93
Michael Tamirie <michaeltamirie@gmail.com>
2022-04-12 10:52:55 +0300

    Finalize the nav links and working fine

---
+++ src/components/MyProfile.js
@@ -0,0 +1,7 @@
+const MyProfile = () => (
+  <div className="my_profile">
+    <h1>This is the profile display page</h1>
+  </div>
+);
+
+export default MyProfile;
---
+++ src/components/Rockets.js
@@ -0,0 +1,7 @@
+const Rockets = () => (
+  <div className="rockets">
+    <h1>This is the rockets display page</h1>
+  </div>
+);
+
+export default Rockets;
