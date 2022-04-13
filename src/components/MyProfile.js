06bfed9edede6f9790e5f474cd8e3384ca0544e9
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-04-13 10:35:05 -0500

    Add list of reserved rockets in my profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,7 +1,27 @@
-const MyProfile = () => (
-  <div className="my_profile">
-    <h1>This is the profile display page</h1>
-  </div>
-);
+import React from 'react';
+import { shallowEqual, useSelector } from 'react-redux';
+
+const MyProfile = () => {
+  const rocketList = useSelector((state) => state.rocket, shallowEqual);
+
+  return (
+    <div className="my_profile">
+      <h1>This is the profile display page</h1>
+      <div>
+        <h2>My Rockets</h2>
+        <ul>
+          {
+          rocketList.filter((rocket) => rocket.reserved)
+            .map((rocket) => (
+              <li key={rocket.id}>
+                {rocket.name}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default MyProfile;
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,14 +1,9 @@
-import React, { useEffect } from 'react';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { shallowEqual, useSelector } from 'react-redux';
 import Rocket from './Rocket';
-import { getRocketList } from '../redux/rockets/rockets';
 
 const Rockets = () => {
   const rocketList = useSelector((state) => state.rocket, shallowEqual);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getRocketList());
-  }, []);
 
   return (
     <div>
